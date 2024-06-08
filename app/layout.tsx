9ffa2e5226424743778d6b90c069efd1e80b71ca
sfayzer <aymenkhaze3.9999@gmail.com>
2024-06-08 19:55:04 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import './globals.css'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins',
+})
+
+export const metadata: Metadata = {
+  title: 'PeakPads',
+  description: 'Created By Aymen & Saad',
+  icons: {
+    icon: '/assets/images/logo.svg',
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={poppins.variable}>{children}</body>
+    </html>
+    </ClerkProvider>
+  )
+}
+
+
+
+
